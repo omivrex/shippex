767fc16c39e84e97cd42a657197e47c165fe6246
omivrex <omivrex@gmail.com>
2024-09-19 17:26:37 +0100

    design login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,8 +1,17 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "../constants/colors.constants";
 
 function Login() {
-    return <SafeAreaView style={styles.container}></SafeAreaView>;
+    return (
+        <SafeAreaView style={styles.container}>
+            <View>
+                <AntDesign name="left" size={24} color="black" />
+                <Text></Text>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -10,7 +19,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#fff",
+        backgroundColor: colors.background,
     },
 });
 
